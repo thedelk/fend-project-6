8a7df2579801f9452dca05f554f2486a833511df
Ryan Delk <ryan@thedelk.com>
2018-09-27 13:36:45 -0500

    [dependency] add prop-types and eslint

--- src/screens/ScreenMain.js
+++ src/screens/ScreenMain.js
@@ -2,52 +2,56 @@ import React, { Component } from "react";
 // import Shelf from "../components/Shelf";
 import * as BooksAPI from "../BooksAPI";
 
-function Book() {
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'
-            }}
-          />
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+class Book extends Component {
+  render() {
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage:
+                  'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
+          <div className="book-title">1776</div>
+          <div className="book-authors">David McCullough</div>
         </div>
-        <div className="book-title">1776</div>
-        <div className="book-authors">David McCullough</div>
-      </div>
-    </li>
-  );
+      </li>
+    );
+  }
 }
 
-function Shelf() {
-  return (
-    <div>
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <Book />
-          </ol>
+class Shelf extends Component {
+  render() {
+    return (
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              <Book />
+            </ol>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default class ScreenMain extends Component {
